463e3d20963557204cd512185fe3f97b9a9f04bc
antoniostassi <stassiant@outlook.it>
2024-07-23 11:40:31 +0200

    JS | Square Creation based to Difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
 
-const difficulty = document.getElementById("difficulty");
 const start = document.getElementById("start");
+const container = document.querySelector(".container");
+
+start.addEventListener("click", function() {
+    
+    const difficulty = document.getElementById("difficulty").value;
+    container.innerHTML = "";
+    
+    for (let i=0; i < difficulty; i++) {
+        console.log("Create "+i);
+        createNewElement("div", "square", container, difficulty, i+1);
+    }
+
+});
+
+
+function createNewElement(tagElement, classToAdd, appendTag, gameDifficulty, numberToDisplay){
+
+    let newElement = document.createElement(tagElement);
+    newElement.classList.add(classToAdd);
+    newElement.classList.add("my-col-"+gameDifficulty);
+    appendTag.append(newElement);
+
+    newElement.addEventListener("click", function() {
+        console.log("Test");
+    });
+
+    newElement.innerHTML = numberToDisplay;
+
+}
