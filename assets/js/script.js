12ee786ba35e3ebfa6b6c94c7523193fb2fcb572
antoniostassi <stassiant@outlook.it>
2024-07-23 11:43:32 +0200

    JS & CSS | Changing bg color on click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,19 @@ const start = document.getElementById("start");
 const container = document.querySelector(".container");
 
 start.addEventListener("click", function() {
-    
+
     const difficulty = document.getElementById("difficulty").value;
     container.innerHTML = "";
     
     for (let i=0; i < difficulty; i++) {
         console.log("Create "+i);
-        createNewElement("div", "square", container, difficulty, i+1);
+        createNewElement("div", "square", container, difficulty, i+1, "new-bg-square");
     }
 
 });
 
 
-function createNewElement(tagElement, classToAdd, appendTag, gameDifficulty, numberToDisplay){
+function createNewElement(tagElement, classToAdd, appendTag, gameDifficulty, numberToDisplay, toggleClass){
 
     let newElement = document.createElement(tagElement);
     newElement.classList.add(classToAdd);
@@ -23,7 +23,7 @@ function createNewElement(tagElement, classToAdd, appendTag, gameDifficulty, num
     appendTag.append(newElement);
 
     newElement.addEventListener("click", function() {
-        console.log("Test");
+        newElement.classList.toggle(toggleClass);
     });
 
     newElement.innerHTML = numberToDisplay;
